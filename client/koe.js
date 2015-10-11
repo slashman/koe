125ad6128104c7ad408c484f21c91540adcce30f
Gustavo Guevara <gustavo.andres.guevara@gmail.com>
2015-10-10 20:20:38 -0500

    changed game start logic on client to support players[D

--- client/koe.js
+++ client/koe.js
@@ -1,3 +1,6 @@
+var waiting = false;
+var socket = false;
+
 function conquer(x,y){
 	if (waiting)
 		return;
@@ -10,4 +13,41 @@ function conquer(x,y){
 		document.getElementById("status").style.color = "green";
 		waiting = false;
 	}, 1000);
+}
+
+function login(){
+	
+	start(document.getElementById("player").value);
+}
+
+function start(playerName){	
+	var color = 'black';
+	var player = {
+		username: playerName
+	};
+	socket = io('http://localhost:3001');
+	socket.emit('getMap', player);
+	socket.on('heresTheMap', function (response){
+		var map = response.map;
+		color = response.color;
+		player.soldiers = response.soldiers;
+		var html = '<table>';
+		for (var y = 0; y < map[0].length; y++){
+			html += '<tr>';
+			for (var x = 0; x < map.length; x++){
+				if (map[x][y].owner)
+					html += '<td id = "cell'+x+'-'+y+'" style = "background-color: '+map[x][y].owner+'" onclick = "conquer('+x+', '+y+')">'+map[x][y].peasants+'</td>'
+				else
+					html += '<td id = "cell'+x+'-'+y+'" style = "background-color: green" onclick = "conquer('+x+', '+y+')">'+map[x][y].peasants+'</td>'
+			}
+			html += '</tr>';
+		}
+		html += '</table>';
+		document.getElementById('playArea').innerHTML = html;
+		document.getElementById('soldiers').innerHTML = player.soldiers;
+	});
+
+	socket.on('conquered', function(conquerInfo){
+		document.getElementById('cell'+conquerInfo.x+"-"+conquerInfo.y).style.backgroundColor = conquerInfo.color;
+	});
 }
